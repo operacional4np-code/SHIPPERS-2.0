2031f6c35c69d5e4f4fc909a130311e514e07de7
operacional4np-code <operacional4np@gmail.com>
2026-05-21 15:20:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -74,9 +74,7 @@ if siglas_input:
     lista_siglas = [s.strip() for s in siglas_input.split(",") if s.strip()]
     
     st.markdown("### 3. Informe a quantidade de sacas para cada destino:")
-    for sigla in lista_siglas:
-        default_val = 17 if sigla == "POA" else 7
-        sacas_manuais[sigla] = st.number_input(f"Sacas para {sigla}:", min_value=0, value=default_val, step=1, key=f"sacas_{sigla}")
+    sacas_manuais[sigla] = st.number_input(f"Sacas para {sigla}:", min_value=0, value=default_val, step=1, key=f"sacas_{sigla}")
 
     # Processamento principal do arquivo
     if file:
